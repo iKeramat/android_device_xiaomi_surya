09d168fecd25e4f0ad06ea98bec45171a2d81ef0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-03 00:30:05 +0200

    surya: Add power AIDL DT2W support via sysfs node
    Change-Id: Idcff758444a5c89ba20f2ae804a407ee78c445cb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,6 +155,7 @@ TARGET_BOARD_PLATFORM := sm6150
 
 # Power
 TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB := //$(DEVICE_PATH):libperfmgr-ext-xiaomi
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
